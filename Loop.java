f5fa3bff8bc18ebd6c13a38398de89e478daf7e6
abijithalagesan <abijithalagesan@gmail.com>
2026-01-12 22:24:52 +0530

    Loop.java

---
+++ Loop.java
@@ -0,0 +1,24 @@
+class Loops {
+    public static void main(String[] args) {
+
+        // for loop
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+
+        // while loop
+        int j = 1;
+        while(j <= 3){
+            System.out.println("While: " + j);
+            j++;
+        }
+
+        // do-while loop
+        int k = 1;
+        do{
+            System.out.println("Do While: " + k);
+            k++;
+        }while(k <= 2);
+
+    }
+}
