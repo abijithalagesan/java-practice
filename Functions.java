8656880e2ad806d49906240cc85297e3c177c431
abijithalagesan <abijithalagesan@gmail.com>
2026-01-12 22:28:16 +0530

    Functions.java

---
+++ Functions.java
@@ -0,0 +1,17 @@
+class Functions {
+
+    static void add(int a, int b){
+        System.out.println("Sum = " + (a + b));
+    }
+
+    static void multiply(int a, int b){
+        System.out.println("Product = " + (a * b));
+    }
+
+    public static void main(String[] args) {
+
+        add(10, 5);
+        multiply(4, 3);
+
+    }
+}
